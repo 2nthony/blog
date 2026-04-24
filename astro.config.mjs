da9a7507352bf1de61157b9dc1c5497bdb17bacb
2nthony <ant.hu@outlook.com>
2026-04-24 17:33:31 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { siteConfig } from "./site.config.mjs";
 
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: siteConfig.site,
+
+  integrations: [sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
